feaa008605fd24b824b303144b59491ac35129d4
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-11-05 11:17:52 +0100

    feat(vps): migrate vps
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/vps/src/index.js
+++ packages/manager/apps/vps/src/index.js
@@ -1,9 +1,18 @@
-import 'script-loader!jquery'; // eslint-disable-line
-import '@ovh-ux/manager-vps';
+/* eslint-disable import/no-webpack-loader-syntax */
+import 'script-loader!jquery';
+import 'script-loader!moment/min/moment-with-locales.min';
+import 'script-loader!jsurl/lib/jsurl';
+/* eslint-enable import/no-webpack-loader-syntax */
 
 import angular from 'angular';
+import '@ovh-ux/manager-vps';
+
+import { momentConfiguration } from './config';
+
+import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
 
 angular
   .module('vpsApp', [
     'ovhManagerVps',
-  ]);
+  ])
+  .config(momentConfiguration);
